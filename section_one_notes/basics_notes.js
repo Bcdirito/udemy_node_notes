897018a1a642ad53541bbfd8c303d86c587c3ed6
Brian DiRito <bdirito91@gmail.com>
2019-06-12 13:31:59 -0400

    added basics notes

---
+++ section_one_notes/basics_notes.js
@@ -0,0 +1,9 @@
+// Node Basics Notes
+
+// To enter REPL (Read, Eval, Print, Loop) -> input node in terminal
+// Node Terminal
+
+// global -> global variable of entire REPL Terminal
+// process -> global object equivalent to document in browser
+
+// To exit -> process.exit()
