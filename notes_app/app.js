d9c63e6d047b5f437781084505c1c8b61549796a
Brian DiRito <bdirito91@gmail.com>
2019-06-12 15:01:21 -0400

    finished importing own files section

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,9 +1,10 @@
+// Part One
 // Load in and save modules to variable
 // Name of variable can be whatever
 // However, what is in require needs to be a node module
 // fs is standard of this name
 // functions with Sync at the end are synchronous, otherwise asynchronous
-const fs = require("fs")
+// const fs = require("fs")
 
 // Creates notes.txt file when didn't exist
 // Else it updated text
@@ -15,4 +16,20 @@ const fs = require("fs")
 // 3. Check your work.
 
 // Similar to reading, writing, appending to files in Python
-fs.appendFileSync("notes.txt", "\nHere is some new text!\n")
+// fs.appendFileSync("notes.txt", "\nHere is some new text!\n")
+
+// Part 2 Importing/Exporting Variables:
+// const add = require("./utils.js")
+
+// const sum = add(4, -2)
+
+// console.log(sum)
+
+// Challenge: Define and use function in a new file
+// 1. Create notes.js
+// 2. Create getNotes function that returns "Your notes..."
+// 3. Expore getNotes function
+// 4. From app.js, load in and call function
+
+const notes = require("./notes.js")
+console.log(notes())
---
+++ notes_app/utils.js
@@ -0,0 +1,8 @@
+console.log("\nutils.js\n")
+
+const name = "Brian\n"
+const add = function(a, b) {
+    return a + b
+}
+
+module.exports = add
