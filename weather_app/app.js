4ad4dd5b79562b8dd047864267ece44399445a2c
Bcdirito <bdirito91@gmail.com>
2019-09-12 13:24:13 -0400

    made basic request of daily forecast and current forecast

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,8 +1,16 @@
 const request = require("request")
 
-const currentLocationURL = "https://api.darksky.net/forecast/a20071b704e2a45e50f3d9ece9208ca0/37.8267,-122.4233"
+const currentLocationURL = "https://api.darksky.net/forecast/a20071b704e2a45e50f3d9ece9208ca0/37.8267,-122.4233?units=si"
 
-request({url: currentLocationURL }, (error, res) => {
-    const data = JSON.parse(res.body)
-    console.log(data.currently)
+// Print current forecast in Celsius
+request({url: currentLocationURL, json: true }, (error, res) => {
+    const currentData = res.body.currently
+    console.log(`It is currently ${currentData.temperature} degrees out. There is a ${currentData.precipProbability}% chance of rain.`)
 })
+
+// Print forecast for today
+
+request({url: currentLocationURL, json: true}, (error, res) => {
+    console.log(res.body.daily.data[0].summary)
+})
+
