eb483a081dcf38231ac663f4b628e97ef11a7dc6
Bcdirito <bdirito91@gmail.com>
2019-09-12 16:28:22 -0400

    added writing for geocoding

--- weather_app/app.js
+++ weather_app/app.js
@@ -2,6 +2,8 @@ const request = require("request")
 
 const currentLocationURL = "https://api.darksky.net/forecast/a20071b704e2a45e50f3d9ece9208ca0/37.8267,-122.4233?units=si"
 
+const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYmRpcml0byIsImEiOiJjazBoNGloN3cwMGM3M21zN251dTR0a3I2In0.HYzT5FP38SY2EEXGvIpdow&limit=1"
+
 // Print current forecast in Celsius
 request({url: currentLocationURL, json: true }, (error, res) => {
     const currentData = res.body.currently
@@ -12,5 +14,19 @@ request({url: currentLocationURL, json: true }, (error, res) => {
 
 request({url: currentLocationURL, json: true}, (error, res) => {
     console.log(res.body.daily.data[0].summary)
+})
+
+// Geocoding
+// Address -> Lat/Long
+
+
+// Print Lat/Long for LA
+request({url: geocodeURL, json: true}, (error, res) => {
+    if (error) console.log(error)
+    else {
+        const latitude = res.body.features[0].center[1]
+        const longitude = res.body.features[0].center[0]
+        console.log(latitude, longitude)
+    }
 })
 
