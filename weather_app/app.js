c9538969009734b401157f61666c34e44fe6df39
Bcdirito <bdirito91@gmail.com>
2019-09-15 15:54:55 -0400

    completed chaining callbacks challenge

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,12 +1,26 @@
 const geocode = require("./utils/geocode")
 const forecast = require("./utils/forecast")
 
-geocode("New York", (error, data) => {
-    if (error) console.log(error)
-    else console.log(data)
-})
+// Accept Location as CLI argument
+// 1. Access cli argument without yargs
+// 2. Use the string vale as the input for geocode
+// 3. Only geocode if location was provided
+// 4. Test work
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    if (error) console.log(error)
-    else console.log(`\n${data}`)
-})
+let input = process.argv[2]
+
+if (!input) console.log ("Please provide address")
+else {
+    geocode(input, (error, data) => {
+        if (error) console.log(error)
+        else {
+            forecast(data.latitude, data.longitude, (error, forecastData) => {
+                if (error) console.log(`\n${error}`)
+                else {
+                    console.log(`\n${data.location}`)
+                    console.log(`\n${forecastData}`)
+                }
+            })
+        }
+    })
+}
--- weather_app/utils/geocode.js
+++ weather_app/utils/geocode.js
@@ -9,7 +9,7 @@ const geocode = (address, callback) => {
             if (body.features.length === 0) callback("Unable to find location. Try another search.", undefined)
             else {
                 callback(undefined, {
-                    latitude: body.features[0].center[0],
+                    latitude: body.features[0].center[1],
                     longitude: body.features[0].center[0],
                     location: body.features[0].place_name
                 })
