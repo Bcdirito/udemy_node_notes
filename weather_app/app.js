d68c6dd7db631159df6b660260256d8b24c97846
Bcdirito <bdirito91@gmail.com>
2019-09-16 09:51:43 -0400

    completed destructuring challenge

--- weather_app/app.js
+++ weather_app/app.js
@@ -3,15 +3,16 @@ const forecast = require("./utils/forecast")
 
 let input = process.argv[2]
 
+
 if (!input) console.log ("Please provide address")
 else {
-    geocode(input, (error, data) => {
+    geocode(input, (error, { latitude, longitude, location } = data) => {
         if (error) console.log(error)
         else {
-            forecast(data.latitude, data.longitude, (error, forecastData) => {
+            forecast(latitude, longitude, (error, forecastData) => {
                 if (error) console.log(`\n${error}`)
                 else {
-                    console.log(`\n${data.location}`)
+                    console.log(`\n${location}`)
                     console.log(`\n${forecastData}`)
                 }
             })
--- weather_app/utils/forecast.js
+++ weather_app/utils/forecast.js
@@ -2,13 +2,13 @@ const request = require("request")
 
 const forecast = (lat, lon, callback) => {
     const url = `https://api.darksky.net/forecast/a20071b704e2a45e50f3d9ece9208ca0/${lat},${lon}`
-    request({url: url, json: true}, (error, res) => {
+    request({url: url, json: true}, (error, {body}) => {
         if (error) callback("Could Not Connect to API", undefined)
         else {
-            const body = res.body
-            if (body.error) callback(body.error, undefined)
+            const {error, currently} = body
+            if (error) callback(error, undefined)
             else {
-                callback(undefined, `It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
+                callback(undefined, `It is currently ${currently.temperature} degrees out. There is a ${currently.precipProbability}% chance of rain.`)
             }
         }
     })
