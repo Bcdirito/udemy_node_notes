489a3f0f79055fa139d26c0cfd7867a25b32b765
Bcdirito <bdirito91@gmail.com>
2019-09-19 10:46:33 -0400

    completed static templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,12 +4,35 @@ const express = require("express")
 const app = express()
 const publicDirectoryPath = path.join(__dirname, "../public") 
 
+app.set("view engine", "hbs")
+
 app.use(express.static(publicDirectoryPath))
 
 app.get("/weather", (req, res) => {
     res.send({
         forecast: "72 degress",
         location: "New York"
+    })
+})
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About Me",
+        name: "Brian DiRito"
+    })
+})
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        title: "Help Page",
+        message: "You are now on the help page."
+    })
+})
+
+app.get("", (req, res) => {
+    res.render("index", {
+        title: "Weather",
+        name: "Brian DiRito"
     })
 })
 
