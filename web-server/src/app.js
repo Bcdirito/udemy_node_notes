793622a85ae28e3627ae6ef107822695dd7959df
Bcdirito <bdirito91@gmail.com>
2019-09-19 11:08:26 -0400

    completed footer and header partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require("path")
 const express = require("express")
+const hbs = require("hbs")
 
 const app = express()
 
 // Defining paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public") 
-const viewsPath = path.join(__dirname, "../templates")
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
 
 // Set up handlebars engine and views location
 app.set("view engine", "hbs")
 app.set("views", viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -31,7 +34,8 @@ app.get("/about", (req, res) => {
 app.get("/help", (req, res) => {
     res.render("help", {
         title: "Help Page",
-        message: "You are now on the help page."
+        message: "You are now on the help page.",
+        name: "Brian DiRito"
     })
 })
 
