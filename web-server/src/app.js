b1846b68997c2cc128a884d4abfa29ca5e23a029
Bcdirito <bdirito91@gmail.com>
2019-09-19 10:52:07 -0400

    organized app.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,10 +2,16 @@ const path = require("path")
 const express = require("express")
 
 const app = express()
+
+// Defining paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public") 
+const viewsPath = path.join(__dirname, "../templates")
 
+// Set up handlebars engine and views location
 app.set("view engine", "hbs")
+app.set("views", viewsPath)
 
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get("/weather", (req, res) => {
