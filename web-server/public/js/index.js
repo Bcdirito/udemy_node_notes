bd1bee21dcb7550e9b510248a4f35f59062a231a
Bcdirito <bdirito91@gmail.com>
2019-09-25 11:14:21 -0400

    fetched weather client side

--- web-server/public/js/index.js
+++ web-server/public/js/index.js
@@ -1,1 +1,8 @@
 console.log("JS File Connected")
+
+fetch("http://localhost:3000/weather?address=boston")
+.then(res => res.json())
+.then(json => {
+    if (json.error) console.log(json.error)
+    else console.log(json.location, json.forecast)
+})
