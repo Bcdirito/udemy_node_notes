4714fc9798aaf3a10252b75001f9ed5801ff5cfd
Bcdirito <bdirito91@gmail.com>
2019-09-25 11:42:42 -0400

    added form for fetch request

--- web-server/public/js/index.js
+++ web-server/public/js/index.js
@@ -1,8 +1,14 @@
-console.log("JS File Connected")
+const getWeatherData = (address) => { 
+    fetch(`http://localhost:3000/weather?address=${address}`)
+    .then(res => res.json())
+    .then(json => {
+        if (json.error) console.log(json.error)
+        else console.log(json.location, json.forecast)
+    })
+}
 
-fetch("http://localhost:3000/weather?address=boston")
-.then(res => res.json())
-.then(json => {
-    if (json.error) console.log(json.error)
-    else console.log(json.location, json.forecast)
+document.getElementById("weatherForm").addEventListener("submit", (e) => {
+    e.preventDefault()
+    getWeatherData(encodeURI(e.target.location.value))
+    e.target.location.value = ""
 })
